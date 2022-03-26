2d7abedbccb7bf317802a56554a313708096efd8
Akme <you@example.com>
2022-03-25 21:56:10 -0400

    duele

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,13 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <ul>
-          <li>   <img class="Logo" src={Logo} /> </li>
+          <li> <img class="Logo" src={Logo} /> </li>
           <li><a href="#news">Ofertas de empleo</a></li>
           <li class="publish"><a href="#Publicar">Publicar un empleo</a></li>
             <input type="text" placeholder="Buscar empleos" name="search"/>
             <input type="button" placeholder='Buscar' name="buttonSearch"/>
-
-
           <li class="rightLogin"><a href="#login">Iniciar sesi&oacute;n</a></li>
           <li class="rightRegister"><a href="#Register">Registrarse</a></li>
         </ul>
@@ -20,9 +18,6 @@ function App() {
         <br></br>
         <br></br>
         <h1 class="Banner">Últimos empleos publicados</h1>
-
-
-
       </header>
     </div>
   );
