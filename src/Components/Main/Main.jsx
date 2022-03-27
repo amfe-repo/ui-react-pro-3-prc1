7a2873968f744cb6763ebd7dc8a7d1816568221c
Akme <you@example.com>
2022-03-27 17:52:34 -0400

    any

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -7,12 +7,12 @@ const Main = (props) => {
         <div className="container-fluid">
             <div className="row">
                 <div className="title text-center p-2 mt-5 text-light bg-info">
-                    <h2 className="fs-1">Cosas de la vida</h2>
+                    <h2 className="fs-1">Últimos empleos publicados</h2>
                 </div>
 
                 <Table></Table>
                 <Table></Table>
-
+                
             </div>
         </div>
     )
